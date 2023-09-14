70310fa25b8d6f36d8f8e06d54a843da62622011
cos12378 <yrias8700@gmail.com>
2023-09-14 17:12:31 +0900

    init

---
+++ src/test/java/com/example/reservationconfigserver/ReservationConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
